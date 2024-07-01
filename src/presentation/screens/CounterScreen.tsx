6ce6e3be959936c4f88aad567122d16f28ed4003
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-01 14:45:01 -0400

    primary button component funcionando

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,26 +1,21 @@
 import { useState } from "react"
-import { Button, Platform, Pressable, StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
+import { PrimaryButton } from "../components"
 
 export const CounterScreen = () => {
 
-const [count, setCount] = useState(10)
+    const [count, setCount] = useState(10)
 
     return (
         <View style={styles.container} >
             <Text style={styles.title} >{count}</Text>
 
-            <Pressable
-                onPress={ () => setCount( count + 1) }
-                onLongPress={ () => setCount(0)}
-                style= {({ pressed }) => [
-                    styles.button,
-                    pressed && styles.buttonPressed
-                ] }
-            >
-                <Text style={{
-                    color: Platform.OS === 'android' ? 'white' : '#4746AB'
-                    }} >Aumentar</Text>
-            </Pressable>
+            <PrimaryButton
+                label='Aumentar'
+                onPress={ () => setCount(count + 1) }
+                onLongPress={ () => setCount(0) }
+            />
+
 
 
         </View>
@@ -38,14 +33,6 @@ const styles = StyleSheet.create({
         fontSize: 45,
         color: 'black',
     },
-    button: {
-        backgroundColor: Platform.OS === 'android' ?  '#5856D6' : 'white',
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        borderRadius: 10
-    },
-    buttonPressed: {
-        backgroundColor: Platform.OS === 'android' ?  '#4746AB' : 'white'
-    }
+
 
 })
