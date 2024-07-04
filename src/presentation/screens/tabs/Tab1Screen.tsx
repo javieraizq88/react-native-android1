c3b3af577fa5a6015c6075d7b3498cc34747ace7
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-04 17:29:33 -0400

    se muestran los iconos de IonIcons

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from "react-native"
 import { HamburgerMenu } from "../../components/shared/HamburgerMenu"
+import Icon from "react-native-vector-icons/Ionicons"
+
+
+
 
 export const Tab1Screen = () => {
     return (
         <View>
             <HamburgerMenu />
+            <Icon name='rocket-outline' size={30} color='#900'/>
             <Text>Tab1Screen</Text>
         </View>
     )
