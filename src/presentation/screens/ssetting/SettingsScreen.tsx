de9cb2be29149b6a4ee7ab3b140fea51f7db67ec
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-04 13:49:58 -0400

    popToTop en settings

--- src/presentation/screens/ssetting/SettingsScreen.tsx
+++ src/presentation/screens/ssetting/SettingsScreen.tsx
@@ -1,9 +1,25 @@
 import { Text, View } from "react-native"
+import { globalStyles } from "../../../config/theme/global.styles"
+import { StackActions, useNavigation } from "@react-navigation/native"
+import { PrimaryButton } from "../../components";
 
-export const SettingsScreen = () =>{
+export const SettingsScreen = () => {
+
+    const navigator = useNavigation();
+
     return (
-        <View>
-            <Text>SettingsScreen</Text>
+        <View style={globalStyles.container} >
+            <Text style={{ marginBottom: 10 }}>SettingsScreen</Text>
+
+            <PrimaryButton
+                label="Volver"
+                onPress={() => navigator.goBack()}
+            />
+      <PrimaryButton
+                label="Home"
+                onPress={() => navigator.dispatch( StackActions.popToTop )}
+            />
+
         </View>
     )
 }
