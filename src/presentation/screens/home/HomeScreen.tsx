f25965f4a489ad10857cccf3f1247da1eb134f1e
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-04 14:52:44 -0400

    toggle drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,22 +1,35 @@
 import { Pressable, Text, View } from "react-native"
 import { globalStyles } from "../../../config/theme/global.styles"
-import { type NavigationProp, useNavigation } from "@react-navigation/native"
+import { type NavigationProp, useNavigation, DrawerActions } from "@react-navigation/native"
 import { PrimaryButton } from "../../components"
-import {type  RootStackParams } from "../../routes/StackNavigation"
+import { type RootStackParams } from "../../routes/StackNavigation"
+import { useEffect } from "react"
 
 export const HomeScreen = () => {
 
     const navigation = useNavigation<NavigationProp<RootStackParams>>();
-    
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <Pressable
+                    onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+                >
+                    <Text>Menu</Text>
+                </Pressable>
+            )
+        })
+    }, [])
+
     return (
         <View style={globalStyles.container} >
             <PrimaryButton
                 label={"Products"}
-                onPress={() => navigation.navigate('Products' )} />
+                onPress={() => navigation.navigate('Products')} />
 
             <PrimaryButton
                 label="Settings"
-                onPress={() => navigation.navigate('Settings' )}
+                onPress={() => navigation.navigate('Settings')}
             />
         </View>
     )
