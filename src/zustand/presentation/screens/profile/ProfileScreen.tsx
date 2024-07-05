e707a5025680c4ee07c1a5911dd5fbb92f8d4db5
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-05 13:36:31 -0400

    setStore

--- src/zustand/presentation/screens/profile/ProfileScreen.tsx
+++ src/zustand/presentation/screens/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import { styles } from "../../../config/app-theme"
 import { useProfileStore } from "../../store/profile-store"
 
@@ -9,10 +9,33 @@ export const ProfileScreen = () => {
     const name = useProfileStore(state => state.name);
     const email = useProfileStore(state => state.email);
 
+    //TODO cambiar el state de name y email
+    const changeProfile = useProfileStore(state => state.changeProfile);
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{name}</Text>
             <Text style={styles.title}>{email}</Text>
+
+            <Pressable
+                style={styles.primaryButton}
+                onPress={() => useProfileStore.setState({ name: 'Javiera' })}
+            >
+                <Text>Cambiar nombre</Text>
+            </Pressable>
+            <Pressable
+                style={styles.primaryButton}
+                onPress={() => useProfileStore.setState({ email: 'cambio el email' })}
+            >
+                <Text>Cambiar email</Text>
+            </Pressable>
+            <Pressable
+                style={styles.primaryButton}
+                onPress={() => changeProfile('Juan', 'correo1')}
+            >
+                <Text>Volver a Juan</Text>
+            </Pressable>
         </View>
     )
 }
--- src/zustand/presentation/store/profile-store.ts
+++ src/zustand/presentation/store/profile-store.ts
@@ -3,9 +3,17 @@ import { create } from 'zustand'
 export interface ProfileState {
     name: string,
     email: string,
+
+    changeProfile: (name: string, email: string) => void;
 }
 
 export const useProfileStore = create<ProfileState>()((set, get) => ({
     name: 'juan',
-    email: 'mail@mail.com'
+    email: 'mail@mail.com',
+
+    changeProfile: (name: string, email: string ) => {
+        console.log(get());
+        // set({name: name, email: email});
+        set({name, email});
+    }
 }));
