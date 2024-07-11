6fd00c069239e1695fab674b6d9358eeba0f62a1
javiera.izquierdo <javiera.izquierdo@MacBook-Pro-de-javieraizquierdo.local>
2024-07-11 13:18:26 -0400

    config para react native

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,7 @@
 import { Text, View } from "react-native"
 import { BottomTabsNavigator } from "./zustand/presentation/navigators/BottomTabsNavigator"
 import { NavigationContainer } from "@react-navigation/native"
+import React from "react"
 
 export const Main =() => {
     return(
